fd673693bb6bcd2ae316d35fa4b397be34bc4593
orathaibaitoey13 <Orathai.baitoey13@gmail.com>
2026-03-11 15:55:32 +0700

    first commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div class= "container">
+    <div class="alert alert-success" role="alert">
+  A simple success alert—check it out!
+</div>
+        <h1>Show Contact</h1>
+        <p>Orathai Tongswangjang</p>
+        <p>Wellcome to vue</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped >
+     h3{
+     color: #2c3e50;
+     }
+</style>
